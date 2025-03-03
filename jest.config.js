f65df19bba2b713f4934102d6cd24d3f4a65a08c
Daniel Vale <danielsvba@hotmail.com>
2025-03-03 18:46:09 -0300

    chore: update Jest setup for react-native-reanimated and adjust Babel config for reanimated plugin

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'react-native',
   transformIgnorePatterns: [
-    'node_modules/(?!(@react-native|react-native|react-native-vector-icons)/)',
+    'node_modules/(?!(@react-native|react-native|react-native-vector-icons|react-native-reanimated)/)',
   ],
   setupFilesAfterEnv: ['./jest.setup.js'],
   modulePathIgnorePatterns: ['mocks'],
--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,3 @@
+import 'react-native-gesture-handler/jestSetup';
 jest.mock('@react-native-vector-icons/Ionicons', () => 'Icon');
+require('react-native-reanimated').setUpTests();
